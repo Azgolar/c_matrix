49f6320f69b9379dda99def0f625fc981933d663
Azgolar <azgolar2@googlemail.com>
2025-05-17 01:32:05 +0200

    formatiert

--- main.c
+++ main.c
@@ -1,197 +1,200 @@
+#include <getopt.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <getopt.h>
 #include <time.h>
-#include <pthread.h>
+#include <unistd.h>
 
 // für Verarbeitung der Eingabe und Ausgabe
 #include "verarbeiten.c"
 
 typedef struct {
-    int anfang;
-    int ende;
-    int n;
-    int **a;
-    int **b;
-    int **c;
+  int anfang;
+  int ende;
+  int n;
+  int **a;
+  int **b;
+  int **c;
 } daten;
 
 void sequentiell(int **a, int **b, int **c, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int summe = 0;
-            for (int k = 0; k < n; k++) {
-                summe = summe + a[i][k] * b[k][j];
-            }
-            c[i][j] = summe;
-        }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int summe = 0;
+      for (int k = 0; k < n; k++) {
+        summe = summe + a[i][k] * b[k][j];
+      }
+      c[i][j] = summe;
     }
+  }
 }
 
-void* parallel_multiplizieren(void *arg) {
-    daten *inhalt = (daten*)arg;
-    for (int i = inhalt->anfang; i < inhalt->ende; i++) {
-        for (int j = 0; j < inhalt->n; j++) {
-            int summe = 0;
-            for (int k = 0; k < inhalt->n; k++) {
-                summe = summe + inhalt->a[i][k] * inhalt->b[k][j];
-            }
-            inhalt->c[i][j] = summe;
-        }
+void *parallel_multiplizieren(void *arg) {
+  daten *inhalt = (daten *)arg;
+  for (int i = inhalt->anfang; i < inhalt->ende; i++) {
+    for (int j = 0; j < inhalt->n; j++) {
+      int summe = 0;
+      for (int k = 0; k < inhalt->n; k++) {
+        summe = summe + inhalt->a[i][k] * inhalt->b[k][j];
+      }
+      inhalt->c[i][j] = summe;
     }
-    return NULL;
+  }
+  return NULL;
 }
 
 void parallel_multiply(int **a, int **b, int **c, int n, int num_threads) {
-    pthread_t threads[num_threads];
-    daten args[num_threads];
-    int base = n / num_threads;
-    int rest  = n % num_threads;
-    int start = 0;
+  pthread_t threads[num_threads];
+  daten args[num_threads];
+  int base = n / num_threads;
+  int rest = n % num_threads;
+  int start = 0;
 
-    for (int i = 0; i < num_threads; i++) {
-		
-		if (rest > 0) {
-			args[i].ende = start + base + 1;
-			rest = rest - 1;
-		} else {
-			args[i].ende = start + base;
-		}
-        args[i].anfang = start;
-        args[i].n      = n;
-        args[i].a      = a;
-        args[i].b      = b;
-        args[i].c      = c;
-        start = args[i].ende;
+  for (int i = 0; i < num_threads; i++) {
+
+    if (rest > 0) {
+      args[i].ende = start + base + 1;
+      rest = rest - 1;
+    } else {
+      args[i].ende = start + base;
     }
-    for (int j = 0; j < num_threads; j++) {
-        pthread_create(&threads[j], NULL, parallel_multiplizieren, &args[j]);
-	}
-    for (int k = 0; k < num_threads; k++) {
-        pthread_join(threads[k], NULL);
-	}
+    args[i].anfang = start;
+    args[i].n = n;
+    args[i].a = a;
+    args[i].b = b;
+    args[i].c = c;
+    start = args[i].ende;
+  }
+  for (int j = 0; j < num_threads; j++) {
+    pthread_create(&threads[j], NULL, parallel_multiplizieren, &args[j]);
+  }
+  for (int k = 0; k < num_threads; k++) {
+    pthread_join(threads[k], NULL);
+  }
 }
 
 int main(int argc, char *argv[]) {
-    char *n_opt = "0";
-    char *datei = NULL;
-    int debug = 1, opt;
+  char *n_opt = "0";
+  char *datei = NULL;
+  int debug = 1, opt;
 
-    while ((opt = getopt(argc, argv, "n:c:dh")) != -1) {
-        switch (opt) {
-            case 'n': n_opt = optarg;          break;
-            case 'c': datei = strdup(optarg);  break;
-            case 'd': debug = 0;               break;
-            case 'h':
-                printf(
-                    "\nOptional:\n"
-                    "-n <zahl>  Matrixgrößen im Bereich [6,zahl] erzeugen\n"
-                    "-c <datei> Ergebnisdatei, Default: matrix.txt\n"
-                    "-d         Debug-Ausgaben\n"
-                );
-                return 0;
-            default:
-                fprintf(stderr, "Unbekannte Option '-%c'. Benutzung siehe -h\n", opt);
-                return 1;
-        }
+  while ((opt = getopt(argc, argv, "n:c:dh")) != -1) {
+    switch (opt) {
+    case 'n':
+      n_opt = optarg;
+      break;
+    case 'c':
+      datei = strdup(optarg);
+      break;
+    case 'd':
+      debug = 0;
+      break;
+    case 'h':
+      printf("\nOptional:\n"
+             "-n <zahl>  Matrixgrößen im Bereich [6,zahl] erzeugen\n"
+             "-c <datei> Ergebnisdatei, Default: matrix.txt\n"
+             "-d         Debug-Ausgaben\n");
+      return 0;
+    default:
+      fprintf(stderr, "Unbekannte Option '-%c'. Benutzung siehe -h\n", opt);
+      return 1;
     }
+  }
 
-    if (!datei) datei = strdup("matrix.txt");
-    if (!strrchr(datei, '.') || strcmp(strrchr(datei, '.'), ".txt") != 0) {
-        char *hinzu = malloc(strlen(datei) + 5);
-        sprintf(hinzu, "%s.txt", datei);
-        free(datei);
-        datei = hinzu;
-    }
-
-    int max_n = strtoul(n_opt, NULL, 10);
-    int *n = NULL, n_laenge = 0;
-    konvertieren(6, max_n, &n, &n_laenge);
-
-    int max_threads = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("Threads: %d\n", max_threads);
-	max_threads = 6;
-	
-    for (int threads = 2; threads <= max_threads; threads++) {
-        double *dauern = malloc(n_laenge * sizeof *dauern);
-
-        for (int i = 0; i < n_laenge; i++) {
-            int aktuell = n[i];
-            int **a =  malloc(aktuell * sizeof(int*));
-            int **b =  malloc(aktuell * sizeof(int*));
-            int **c =  malloc(aktuell * sizeof(int*));
-			
-			// Speicher für Reihen allokieren
-			for (int i = 0; i < aktuell; i++) {
-				a[i] = malloc(aktuell * sizeof *a[i]);
-				b[i] = malloc(aktuell * sizeof *b[i]);
-				c[i] = malloc(aktuell * sizeof *c[i]);
-			}
-
-
-            /* Initialisieren */
-            for (int i = 0; i < aktuell; i++)
-                for (int j = 0; j < aktuell; j++) {
-                    a[i][j] = rand();
-                    b[i][j] = rand();
-                    c[i][j] = 0;
-                }
-
-            /* Zeitmessung */
-            struct timespec t0, t1;
-            clock_gettime(CLOCK_MONOTONIC, &t0);
-            parallel_multiply(a, b, c, aktuell, threads);
-            clock_gettime(CLOCK_MONOTONIC, &t1);
-
-            dauern[i] = (t1.tv_sec - t0.tv_sec)*1e3
-                        + (t1.tv_nsec - t0.tv_nsec)/1e6;
-
-            if (debug) {
-                int **c_sequentiell = malloc(aktuell * sizeof *c_sequentiell);
-				for (int r = 0; r < aktuell; r++) {
-					c_sequentiell[r] = malloc(aktuell * sizeof *c_sequentiell[r]);
-				}
-                sequentiell(a, b, c_sequentiell, aktuell);
-
-                int fehler = 0;
-                for (int i = 0; i < aktuell && !fehler; i++) {
-                    for (int j = 0; j < aktuell; j++) {
-                        if (c_sequentiell[i][j] != c[i][j]) {
-                            fehler = 1;
-                            break;
-                        }
-                    }
-                }
-				if (fehler == 0) {
-					printf("Ergebnis ist korrekt\n");
-				}
-				else {
-                    printf("Ergebnis ist falsch");
-					
-                for (int i = 0; i < aktuell; ++i) {
-					free(c_sequentiell[i]); 
-				}
-				free(c_sequentiell); 
-				}
-            }
-
-            for (int i = 0; i < aktuell; ++i) {
-				free(a[i]);
-				free(b[i]); 
-				free(c[i]); 				
-			}
-			free(a);
-			free(b);
-			free(c);
-        }
-
-        speichern(datei, n, dauern, n_laenge, threads);
-        free(dauern);
-    }
-
-    free(n);
+  if (!datei)
+    datei = strdup("matrix.txt");
+  if (!strrchr(datei, '.') || strcmp(strrchr(datei, '.'), ".txt") != 0) {
+    char *hinzu = malloc(strlen(datei) + 5);
+    sprintf(hinzu, "%s.txt", datei);
     free(datei);
-    return 0;
+    datei = hinzu;
+  }
+
+  int max_n = strtoul(n_opt, NULL, 10);
+  int *n = NULL, n_laenge = 0;
+  konvertieren(6, max_n, &n, &n_laenge);
+
+  int max_threads = sysconf(_SC_NPROCESSORS_ONLN);
+  printf("Threads: %d\n", max_threads);
+  max_threads = 6;
+
+  for (int threads = 2; threads <= max_threads; threads++) {
+    double *dauern = malloc(n_laenge * sizeof *dauern);
+
+    // Speicher für Matrix allokieren
+    for (int i = 0; i < n_laenge; i++) {
+      int aktuell = n[i];
+      int **a = malloc(aktuell * sizeof(int *));
+      int **b = malloc(aktuell * sizeof(int *));
+      int **c = malloc(aktuell * sizeof(int *));
+
+      for (int i = 0; i < aktuell; i++) {
+        a[i] = malloc(aktuell * sizeof *a[i]);
+        b[i] = malloc(aktuell * sizeof *b[i]);
+        c[i] = malloc(aktuell * sizeof *c[i]);
+      }
+
+      /* Initialisieren */
+      for (int i = 0; i < aktuell; i++)
+        for (int j = 0; j < aktuell; j++) {
+          a[i][j] = rand();
+          b[i][j] = rand();
+          c[i][j] = 0;
+        }
+
+      /* Zeitmessung */
+      struct timespec t0, t1;
+      clock_gettime(CLOCK_MONOTONIC, &t0);
+      parallel_multiply(a, b, c, aktuell, threads);
+      clock_gettime(CLOCK_MONOTONIC, &t1);
+
+      dauern[i] =
+          (t1.tv_sec - t0.tv_sec) * 1e3 + (t1.tv_nsec - t0.tv_nsec) / 1e6;
+
+      if (debug) {
+        int **c_sequentiell = malloc(aktuell * sizeof *c_sequentiell);
+        for (int r = 0; r < aktuell; r++) {
+          c_sequentiell[r] = malloc(aktuell * sizeof *c_sequentiell[r]);
+        }
+        sequentiell(a, b, c_sequentiell, aktuell);
+
+        int fehler = 0;
+        for (int i = 0; i < aktuell && !fehler; i++) {
+          for (int j = 0; j < aktuell; j++) {
+            if (c_sequentiell[i][j] != c[i][j]) {
+              fehler = 1;
+              break;
+            }
+          }
+        }
+        if (fehler == 0) {
+          printf("Ergebnis ist korrekt\n");
+        } else {
+          printf("Ergebnis ist falsch");
+
+          for (int i = 0; i < aktuell; ++i) {
+            free(c_sequentiell[i]);
+          }
+          free(c_sequentiell);
+        }
+      }
+
+      for (int i = 0; i < aktuell; ++i) {
+        free(a[i]);
+        free(b[i]);
+        free(c[i]);
+      }
+      free(a);
+      free(b);
+      free(c);
+    }
+
+    speichern(datei, n, dauern, n_laenge, threads);
+    free(dauern);
+  }
+
+  free(n);
+  free(datei);
+  return 0;
 }
--- verarbeiten.c
+++ verarbeiten.c
@@ -1,140 +1,140 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/stat.h>
-#include <errno.h>
+#include <unistd.h>
 
-// Erzeugt ein nay mit Zahn_laenge im Bereich [start, ende] mit adaptiver Schrittweite
-void konvertieren(int start, int ende, int **n, int *n_laenge)
-{
-	if (ende == 0) {
-		printf("Fehler: -n <zahl> muss angegeben sein. Benutzung siehe -h\n");
-        exit(1);
-    }
-	
-    if (ende < 6) {
-        printf("Fehler: -n muss >= 6 sein\n");
-        exit(1);
-    }
-	
-	// maximal 100 Messwerte
-    int *zwischen = malloc(100 * sizeof(int));
+// Erzeugt ein nay mit Zahn_laenge im Bereich [start, ende] mit adaptiver
+// Schrittweite
+void konvertieren(int start, int ende, int **n, int *n_laenge) {
+  if (ende == 0) {
+    printf("Fehler: -n <zahl> muss angegeben sein. Benutzung siehe -h\n");
+    exit(1);
+  }
 
-    int letztes = start;
-    int zweier = 1;
-    
-	int i = 0;
-    zwischen[i] = start;
-    i = i + 1;
+  if (ende < 6) {
+    printf("Fehler: -n muss >= 6 sein\n");
+    exit(1);
+  }
 
-	int schritt;
-	int aktuell;
-	
-    while (letztes < ende) {
-        if      (letztes <= 9)     schritt = 4;
-        else if (letztes <= 99)    schritt = 6;
-        else if (letztes <= 999)   schritt = 100;
-        else if (letztes <= 9999)  schritt = 500;
-        else                       schritt = 1000;
+  // maximal 100 Messwerte
+  int *zwischen = malloc(100 * sizeof(int));
 
-        aktuell = letztes + schritt;
+  int letztes = start;
+  int zweier = 1;
 
-		// Matrixen mit 2^x Größe immer hinzufügen
-		if (zweier > letztes && zweier <= ende) {
-			if (zweier < aktuell) {
-				zwischen[i] = zweier;
-				i = i + 1;
-				letztes = zweier;
-				zweier = zweier * 2;
-				continue;
-			}
-		}
+  int i = 0;
+  zwischen[i] = start;
+  i = i + 1;
 
-        if (aktuell >= ende) {
-            zwischen[i] = ende;
-            i = i + 1;
-            break;
-        }
+  int schritt;
+  int aktuell;
 
-        zwischen[i] = aktuell;
+  while (letztes < ende) {
+    if (letztes <= 9)
+      schritt = 4;
+    else if (letztes <= 99)
+      schritt = 6;
+    else if (letztes <= 999)
+      schritt = 100;
+    else if (letztes <= 9999)
+      schritt = 500;
+    else
+      schritt = 1000;
+
+    aktuell = letztes + schritt;
+
+    // Matrixen mit 2^x Größe immer hinzufügen
+    if (zweier > letztes && zweier <= ende) {
+      if (zweier < aktuell) {
+        zwischen[i] = zweier;
         i = i + 1;
-        letztes = aktuell;
-		
-		if (i > 100)
-		{
-			printf("\n darf maximal 100 Messwerte haben\n");
-			break;
-		}
+        letztes = zweier;
+        zweier = zweier * 2;
+        continue;
+      }
     }
 
-    *n = zwischen;
-    *n_laenge = i;
+    if (aktuell >= ende) {
+      zwischen[i] = ende;
+      i = i + 1;
+      break;
+    }
+
+    zwischen[i] = aktuell;
+    i = i + 1;
+    letztes = aktuell;
+
+    if (i > 100) {
+      printf("\n darf maximal 100 Messwerte haben\n");
+      break;
+    }
+  }
+
+  *n = zwischen;
+  *n_laenge = i;
 }
 
-
 // Speichert Prozessorinfo und Messdaten in eine Datei
-void speichern(const char *dateiname, int *n, double *laufzeit, int n_laenge, int threads) {
-	FILE *cpu = fopen("/proc/cpuinfo", "r");
+void speichern(const char *dateiname, int *n, double *laufzeit, int n_laenge,
+               int threads) {
+  FILE *cpu = fopen("/proc/cpuinfo", "r");
 
-	char line[256];
-	char name[80] = "";
-	int logical = 0, physical = 0;
+  char line[256];
+  char name[80] = "";
+  int logical = 0, physical = 0;
 
-	// Lesen aller zein_laenge
-	while (fgets(line, sizeof(line), cpu)) {
-		// Prozessormodell lesen
-		if (!name[0] && strncmp(line, "model name", 10) == 0) {
-			char *p = strchr(line, ':') + 2;  
-			strcpy(name, p);
-			name[strcspn(name, "\n")] = '\0';
-		}
-		// vorkommen "processor" ist identisch zur Anzahl der logischen Kerne 
-		if (strncmp(line, "processor", 9) == 0) {
-			logical = logical + 1;
-		}
-		// Anzahl der physischen Kerne aus Zeile lesen
-		if (!physical && strncmp(line, "cpu cores", 9) == 0) {
-			physical = atoi(strchr(line, ':') + 1);
-		}
-	}
-
-	fclose(cpu);
-
-	int hyperthreading = 0;
-	if (physical > 0)
-	{
-		hyperthreading = logical / physical;
-	}
-
-    // Prüfen, ob Datei existiert
-	// stat ist in stat.h enthalt und hat die Metadaten der Datei
-    struct stat metadaten;
-    int exists = (stat(dateiname, &metadaten) == 0);
-
-    FILE *f = fopen(dateiname, "a");
-    if (!f) {
-        printf("Fehler beim Öffnen der Datei zum Speichern der Daten");
-        exit(1);
+  // Lesen aller zein_laenge
+  while (fgets(line, sizeof(line), cpu)) {
+    // Prozessormodell lesen
+    if (!name[0] && strncmp(line, "model name", 10) == 0) {
+      char *p = strchr(line, ':') + 2;
+      strcpy(name, p);
+      name[strcspn(name, "\n")] = '\0';
     }
-
-    // Kopfzeile nur schreiben, wenn Datei neu
-    if (!exists) {
-        fprintf(f, "%s,%u,%u,%u\n", dateiname, logical, physical, hyperthreading);
+    // vorkommen "processor" ist identisch zur Anzahl der logischen Kerne
+    if (strncmp(line, "processor", 9) == 0) {
+      logical = logical + 1;
     }
-
-    // Messdaten anhängen
-    for (int i = 0; i < n_laenge; i++) {
-        fprintf(f, "%u,%u,%.6f\n", threads, n[i], laufzeit[i]);
+    // Anzahl der physischen Kerne aus Zeile lesen
+    if (!physical && strncmp(line, "cpu cores", 9) == 0) {
+      physical = atoi(strchr(line, ':') + 1);
     }
-    fclose(f);
+  }
 
-    if (exists)
-	{
-		printf("Daten erfolgreich angehängt an %s.\n", dateiname);
-	}
-    else
-	{
-		printf("Daten erfolgreich in %s geschrieben.\n", dateiname);
-	}
+  fclose(cpu);
+
+  int hyperthreading = 0;
+  if (physical > 0) {
+    hyperthreading = logical / physical;
+  }
+
+  // Prüfen, ob Datei existiert
+  // stat ist in stat.h enthalt und hat die Metadaten der Datei
+  struct stat metadaten;
+  int exists = (stat(dateiname, &metadaten) == 0);
+
+  FILE *f = fopen(dateiname, "a");
+  if (!f) {
+    printf("Fehler beim Öffnen der Datei zum Speichern der Daten");
+    exit(1);
+  }
+
+  // Kopfzeile nur schreiben, wenn Datei neu
+  if (!exists) {
+    fprintf(f, "%s,%u,%u,%u\n", dateiname, logical, physical, hyperthreading);
+  }
+
+  // Messdaten anhängen
+  for (int i = 0; i < n_laenge; i++) {
+    fprintf(f, "%u,%u,%.6f\n", threads, n[i], laufzeit[i]);
+  }
+  fclose(f);
+
+  if (exists) {
+    printf("Daten erfolgreich angehängt an %s.\n", dateiname);
+  } else {
+    printf("Daten erfolgreich in %s geschrieben.\n", dateiname);
+  }
 }
